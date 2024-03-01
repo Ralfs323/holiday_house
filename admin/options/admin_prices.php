cc6e7387c7524847f86559de95d530f6d2dfb256
Ralfs323 <ip20.r.rutkovskis@vtdt.edu.lv>
2024-03-01 13:30:19 +0200

    admin sadaļas
    pievienoju katrai sadaļai pārbaudi vai ir lietotājs ir autentificējies un vai ir admins.

--- admin/options/admin_prices.php
+++ admin/options/admin_prices.php
@@ -1,7 +1,19 @@
 <?php
 include "../../db/db.php";
-include "auth.php";
+session_start();
 
+// Pārbaudām, vai lietotājs ir autentificējies
+if (!isset($_SESSION['user_id'])) {
+    header("Location: /auth/login.php"); // Ja lietotājs nav autentificējies, novirzīt uz autentifikācijas lapu
+    exit;
+}
+
+// Pārbaudām, vai lietotājs ir administrators
+if (!isset($_SESSION['is_admin']) || $_SESSION['is_admin'] !== true) {
+    // Ja lietotājs nav administrators, novirzīt uz kļūdas lapu vai citu atbilstošu vietni
+    header("Location: /error.php");
+    exit;
+}
 
 // Check if form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
