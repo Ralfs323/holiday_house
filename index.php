9d62d943bde5a2e839b23b08a58392d63d818f99
Ralfs323 <ip20.r.rutkovskis@vtdt.edu.lv>
2024-06-07 11:24:33 +0300

    updated
    updated

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ if (isset($_GET['success']) && $_GET['success'] == 1) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hotel Website</title>
+    <title>Holiday house Estere</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
     <link rel="stylesheet" href="style.css">
